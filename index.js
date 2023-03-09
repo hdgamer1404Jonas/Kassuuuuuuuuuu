4faf34eef9a0ab2a0af59224bd27d8a9329fcb8d
hdgamer1404Jonas <hdgamer1404Jonas@gmail.com>
2023-03-09 17:45:54 +0100

    Added Duplicate Check

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ async function download() {
             const url = json.url;
             const filename = url.split('/').pop();
 
+            if (fs.existsSync('./nekos/' + filename)) {
+                console.log('File already exists');
+                return;
+            }
+
             https.get(url, (res) => {
                 res.pipe(fs.createWriteStream("./nekos/" + filename));
                 console.log('Downloaded ' + filename);
